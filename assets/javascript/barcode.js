2b686560b494e081347792842a00021b637521de
davesrose <dsr0116@yahoo.com>
2017-06-15 21:18:28 -0400

    revert

--- assets/javascript/barcode.js
+++ assets/javascript/barcode.js
@@ -40,7 +40,6 @@ $(function() {
 			function(err) {
 				if (err) {
 					$('#livestream_scanner .modal-body .error').html('<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> '+err.name+'</strong>: '+err.message+'</div>');
-					
 					Quagga.stop();
 					return;
 				}
