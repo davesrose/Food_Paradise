fa7f65a0c56fa21f6a79af78731ebda3171a9253
gtbmed <jkbarson@gmail.com>
2017-06-18 17:29:48 -0400

    adding correct UA code and current DB accessing code

--- assets/javascript/uaDBstore.js
+++ assets/javascript/uaDBstore.js
@@ -1,19 +1,19 @@
 //User Authentication and database storage
-$(document).ready(function() {
+//$(document).ready(function() {
   // Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyDi2g588bcWLXFwdCjviNcxOLMGQapxjbU",
-		authDomain: "food-paradise-8ef13.firebaseapp.com",
-		databaseURL: "https://food-paradise-8ef13.firebaseio.com",
-		projectId: "food-paradise-8ef13",
-		storageBucket: "food-paradise-8ef13.appspot.com",
-		messagingSenderId: "698241980106"
-	};
-	firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyDi2g588bcWLXFwdCjviNcxOLMGQapxjbU",
+    authDomain: "food-paradise-8ef13.firebaseapp.com",
+    databaseURL: "https://food-paradise-8ef13.firebaseio.com",
+    projectId: "food-paradise-8ef13",
+    storageBucket: "food-paradise-8ef13.appspot.com",
+    messagingSenderId: "698241980106"
+  };
+  firebase.initializeApp(config);
 
-	// FirebaseUI config.
+  // FirebaseUI config.
       var uiConfig = {
-        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInSuccessUrl: 'https://davesrose.github.io/Food_Paradise/',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -27,5 +27,16 @@ $(document).ready(function() {
       var ui = new firebaseui.auth.AuthUI(firebase.auth());
       // The start method will wait until the DOM is loaded.
       ui.start('#firebaseui-auth-container', uiConfig);
-	});
-});
+
+  //User database additions
+  var database = firebase.database();
+
+  $(".save").on("click", function(event) {
+    event.preventDefault();
+    recipeId = $("#train-name").val().trim();
+
+    database.ref().push({
+        recipeId: recipeId,
+    });
+    }
+//});
