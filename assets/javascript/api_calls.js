e69fe8fe8da28e5ca59a0a6d98e0a8353a96fa3b
gtbmed <jkbarson@gmail.com>
2017-06-18 11:05:44 -0400

    Work so far

---
+++ assets/javascript/api_calls.js
@@ -0,0 +1,11 @@
+
+// Nutritionix api call format
+https://api.nutritionix.com/v1_1/item?upc=049000040869&appId=2f5e1efc&appKey=5bb13eceb17d1652e94098898e092a2e
+
+//Yummly api call format
+http://api.yummly.com/v1/api/recipes?_app_id=069691a5&_app_key=app-key&your _search_parameters
+
+http://api.yummly.com/v1/api/recipe/Golden-Raisin-Rosemary-Oatmeal-Cookies-1785227?_app_id=069691a5&_app_key=3944fb993fe2cb009e5e6a5fd1e4facb
+
+http://api.yummly.com/v1/api/recipes?_app_id=069691a5&_app_key=3944fb993fe2cb009e5e6a5fd1e4facb&Golden+Raisins
+
