1f324ba91310b7806672293ae637b1456dc93369
davesrose <dsr0116@yahoo.com>
2017-06-17 20:35:11 -0400

    working behaviors

--- assets/javascript/barcode.js
+++ assets/javascript/barcode.js
@@ -1,135 +1,158 @@
-$(function() {
-	// Create the QuaggaJS config object for the live stream
-	var liveStreamConfig = {
-			inputStream: {
-				type : "LiveStream",
-				constraints: {
-					width: {min: 640},
-					height: {min: 480},
-					aspectRatio: {min: 1, max: 100},
-					facingMode: "environment" // or "user" for the front camera
-				}
-			},
-			locator: {
-				patchSize: "medium",
-				halfSample: true
-			},
-			numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
-			decoder: {
-				"readers":[
-					{"format":"ean_reader","config":{}}
-				]
-			},
-			locate: true
-		};
-	// The fallback to the file API requires a different inputStream option. 
-	// The rest is the same 
-	var fileConfig = $.extend(
-			{}, 
-			liveStreamConfig,
-			{
+$(document).ready(function(){
+	if (localStorage.getItem("foodName") !== null) {
+		var foodName = localStorage.getItem("foodName");
+		yummlyResponse(foodName);
+	}
+	$(function() {
+		// Create the QuaggaJS config object for the live stream
+		var liveStreamConfig = {
 				inputStream: {
-					size: 800
+					type : "LiveStream",
+					constraints: {
+						width: {min: 640},
+						height: {min: 480},
+						aspectRatio: {min: 1, max: 100},
+						facingMode: "environment" // or "user" for the front camera
+					}
+				},
+				locator: {
+					patchSize: "medium",
+					halfSample: true
+				},
+				numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
+				decoder: {
+					"readers":[
+						{"format":"ean_reader","config":{}}
+					]
+				},
+				locate: true
+			};
+		// The fallback to the file API requires a different inputStream option. 
+		// The rest is the same 
+		var fileConfig = $.extend(
+				{}, 
+				liveStreamConfig,
+				{
+					inputStream: {
+						size: 800
+					}
+				}
+			);
+		// Start the live stream scanner when the modal opens
+		$('#livestream_scanner').on('shown.bs.modal', function (e) {
+			Quagga.init(
+				liveStreamConfig, 
+				function(err) {
+					if (err) {
+						$("#interactive").hide();
+						if (err.name === "DevicesNotFoundError") {
+							$('#livestream_scanner .modal-body .error').html("Camera not found.  Press 'Use camera app' button to find a photo of a barcode.")
+						} else if (err.message === "getUserMedia is not defined") {
+							$('#livestream_scanner .modal-body .error').html("Press 'Use camera app' to grab a bardcode from your phone's camera");
+						} else {
+							$('#livestream_scanner .modal-body .error').html('<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> '+err.name+'</strong>: '+err.message+'</div>');
+						}
+						Quagga.stop();
+						return;
+					}
+					Quagga.start();
+				}
+			);
+	    });
+		
+		// Make sure, QuaggaJS draws frames an lines around possible 
+		// barcodes on the live stream
+		Quagga.onProcessed(function(result) {
+			var drawingCtx = Quagga.canvas.ctx.overlay,
+				drawingCanvas = Quagga.canvas.dom.overlay;
+	 
+			if (result) {
+				if (result.boxes) {
+					drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+					result.boxes.filter(function (box) {
+						return box !== result.box;
+					}).forEach(function (box) {
+						Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+					});
+				}
+	 
+				if (result.box) {
+					Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+				}
+	 
+				if (result.codeResult && result.codeResult.code) {
+					Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
 				}
 			}
-		);
-	// Start the live stream scanner when the modal opens
-	$('#livestream_scanner').on('shown.bs.modal', function (e) {
-		Quagga.init(
-			liveStreamConfig, 
-			function(err) {
-				if (err) {
-					$('#livestream_scanner .modal-body .error').html('<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> '+err.name+'</strong>: '+err.message+'</div>');
-					Quagga.stop();
-					return;
-				}
-				Quagga.start();
+		});
+		
+		// Once a barcode had been read successfully, stop quagga and 
+		// close the modal after a second to let the user notice where 
+		// the barcode had actually been found.
+		Quagga.onDetected(function(result) {    		
+			if (result.codeResult.code){
+				$('#scanner_input').val(result.codeResult.code);
+				Quagga.stop();	
+				setTimeout(function(){ $('#livestream_scanner').modal('hide'); }, 1000);
+				var UPC = result.codeResult.code;
+				getIDs(UPC);			
 			}
-		);
-    });
-	
-	// Make sure, QuaggaJS draws frames an lines around possible 
-	// barcodes on the live stream
-	Quagga.onProcessed(function(result) {
-		var drawingCtx = Quagga.canvas.ctx.overlay,
-			drawingCanvas = Quagga.canvas.dom.overlay;
- 
-		if (result) {
-			if (result.boxes) {
-				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-				result.boxes.filter(function (box) {
-					return box !== result.box;
-				}).forEach(function (box) {
-					Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-				});
+		});
+	    
+		// Stop quagga in any case, when the modal is closed
+	    $('#livestream_scanner').on('hide.bs.modal', function(){
+	    	if (Quagga){
+	    		Quagga.stop();	
+	    	}
+	    });
+		
+		// Call Quagga.decodeSingle() for every file selected in the 
+		// file input
+		$("#livestream_scanner input:file").on("change", function(e) {
+			if (e.target.files && e.target.files.length) {
+				Quagga.decodeSingle($.extend({}, fileConfig, {src: URL.createObjectURL(e.target.files[0])}), function(result) {alert(result.codeResult.code);});
+				var UPC = result.codeResult.code;
+				getIDs(UPC);
 			}
- 
-			if (result.box) {
-				Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-			}
- 
-			if (result.codeResult && result.codeResult.code) {
-				Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-			}
-		}
+		});
 	});
-	
-	// Once a barcode had been read successfully, stop quagga and 
-	// close the modal after a second to let the user notice where 
-	// the barcode had actually been found.
-	Quagga.onDetected(function(result) {    		
-		if (result.codeResult.code){
-			$('#scanner_input').val(result.codeResult.code);
-			Quagga.stop();	
-			setTimeout(function(){ $('#livestream_scanner').modal('hide'); }, 1000);
-			var UPC = result.codeResult.code;
-			getIDs(UPC);			
-		}
-	});
-    
-	// Stop quagga in any case, when the modal is closed
-    $('#livestream_scanner').on('hide.bs.modal', function(){
-    	if (Quagga){
-    		Quagga.stop();	
-    	}
-    });
-	
-	// Call Quagga.decodeSingle() for every file selected in the 
-	// file input
-	$("#livestream_scanner input:file").on("change", function(e) {
-		if (e.target.files && e.target.files.length) {
-			Quagga.decodeSingle($.extend({}, fileConfig, {src: URL.createObjectURL(e.target.files[0])}), function(result) {alert(result.codeResult.code);});
-			var UPC = result.codeResult.code;
-			getIDs(UPC);
-		}
-	});
-});
-function getIDs(UPC) {
-	$(".barcodeNum").html("UPC: " + UPC);
-	var queryURL = "https://api.nutritionix.com/v1_1/item?upc="+UPC+"&appId=af71ef2f&appKey=64be45970143817635f29340426218f7";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-      console.log(queryURL);
-      console.log(response);
-      var foodName = response.item_name.replace(/,/g , '');;
-      $(".foodID").html("food name: " + foodName);
-      yummlyResponse(foodName);
-    });
-    function yummlyResponse(foodName) {
-    	var queryURL2 = "https://api.yummly.com/v1/api/recipes?_app_id=069691a5&_app_key=3944fb993fe2cb009e5e6a5fd1e4facb&q="+foodName+"&requirePictures=true&maxResult=10&start=10"
+	function getIDs(UPC) {
+		$(".barcodeNum").html("UPC: " + UPC);
+		var queryURL = "https://api.nutritionix.com/v1_1/item?upc="+UPC+"&appId=af71ef2f&appKey=64be45970143817635f29340426218f7";
 	    $.ajax({
-	      url: queryURL2,
+	      url: queryURL,
 	      method: "GET"
 	    }).done(function(response) {
 	      console.log(queryURL);
 	      console.log(response);
+	      var foodName = response.item_name.replace(/,/g , '');;
+	      $(".foodID").html("food name: " + foodName);
+	      localStorage.setItem("foodName",foodName);
+	      yummlyResponse(foodName);
+	    });
+	}
+	function yummlyResponse(foodName) {
+		$(".yummlyRecipes").empty();
+		var queryURL2 = "https://api.yummly.com/v1/api/recipes?_app_id=069691a5&_app_key=3944fb993fe2cb009e5e6a5fd1e4facb&q="+foodName+"&requirePictures=true&maxResult=10&start=10"
+	    $.ajax({
+	      url: queryURL2,
+	      method: "GET"
+	    }).done(function(response) {
+	      console.log(queryURL2);
+	      console.log(response);
 	      for (var i = 0; i < response.matches.length; i++) {
 	      	var foodItems = response.matches[i].id
-	      	var foodListings = ("<div>Yummly Recipe Name: " + foodItems + "</div>");
+	      	var recipeName = response.matches[i].recipeName
+	      	var foodListings = ("<div data-id='"+foodItems+"' class='recipeOption'><div class='recipeThumb'>" + recipeName + "</div></div>");
 	      	$(".yummlyRecipes").append(foodListings);
 	      }
 	    });
-    }
-}
+	}
+	$(document).on("click", "div.recipeOption" , function() {
+		var recipeID = $(this).data("id");
+		console.log(recipeID);
+		localStorage.setItem("recipeID", recipeID);
+		console.log(localStorage.getItem("recipeID"));
+		window.location.href = "recipe.html";
+	});
+});
---
+++ assets/javascript/recipe.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	// var recipeID = "Golden-Raisin-Rosemary-Oatmeal-Cookies-1785227"
+	var recipeID = localStorage.getItem("recipeID");
+	var queryURL = "http://api.yummly.com/v1/api/recipe/"+recipeID+"?_app_id=069691a5&_app_key=3944fb993fe2cb009e5e6a5fd1e4facb"
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(queryURL);
+      console.log(response);
+      var recipeName = response.name;
+      var servingNum = response.numberOfServings;
+      var totalTime = response.totalTime;
+      var rating = Math.round(response.rating);
+      var img = response.images[0].hostedLargeUrl;
+      for (var j = 0; j < response.nutritionEstimates.length; j++) {
+      	if (response.nutritionEstimates[j].description === "Potassium, K") {
+      		var potassium = Math.round(response.nutritionEstimates[j].value*1000);
+      	}
+      	if (response.nutritionEstimates[j].description === "Protein") {
+      		var protein = Math.round(response.nutritionEstimates[j].value);
+      	}      	
+       	if (response.nutritionEstimates[j].description === "Total lipid (fat)") {
+      		var fat = Math.round(response.nutritionEstimates[j].value);
+      	}  
+       	if (response.nutritionEstimates[j].description === "Sodium, Na") {
+      		var sodium = Math.round(response.nutritionEstimates[j].value*1000); // grams
+      	}
+       	if (response.nutritionEstimates[j].description === "Carbohydrate, by difference") {
+      		var carbs = Math.round(response.nutritionEstimates[j].value);
+      	}
+        if (response.nutritionEstimates[j].description === "Fiber, total dietary") {
+      		var fiber = Math.round(response.nutritionEstimates[j].value);
+      	}
+      }
+      var cals = Math.round(9*fat+4*carbs+4*protein);
+      var recipeSource = response.source.sourceRecipeUrl;
+	  for (var i = 0; i < response.ingredientLines.length; i++) {
+      	var ingredients = response.ingredientLines[i]
+      	console.log(ingredients);
+      	$(".recipeList > ul").append("<li>"+ingredients+"</li>")
+      }
+      $(".name").html(recipeName);
+      $(".img").attr("src",img);
+      $(".info").html("<div class='rate'><img src='assets/images/"+rating+"stars.png'></div><div class='time'>"+totalTime+"</div><div class='servings'>Serves: "+servingNum+"</div><div class'clear'></div>");
+      $(".nutrition").html("<div class='nut' style='margin-right:3%; float: left;'>Calories:"+cals+"</div><div class'nut' style='margin-right:3%; float: left;'>Sodium:"+sodium+"mg</div><div class'nut' style='margin-right:3%; float: left;'>Carbs:"+carbs+"g</div><div class'nut' style='margin-right:3%; float: left;'>Fat:"+fat+"g</div><div class'nut' style='margin-right:3%; float: left;'>Protein:"+protein+"g</div><div class'nut' style='margin-right:3%; float: left;'>Fiber:"+fiber+"g</div><div class'nut' style='margin-right:3%; float: left;'>Potassium:"+potassium+"mg</div><div class='clear'></div>");
+      $("#recipeModal").on("click", function() {
+      		$(".directionsHTML").html("<iframe src='"+recipeSource+"'></iframe>");
+  		});
+      console.log("Cals: "+cals+" sodium: "+sodium+" fat: "+fat+" carbs: "+carbs+" protein: "+protein+" potassium: "+potassium);
+    });
+});
