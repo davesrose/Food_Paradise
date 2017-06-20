c550645c8bf061e9628f5f0bf01ddbb22ad1127a
John Barson <John@NewAdventure.(none)>
2017-06-20 19:09:40 -0400

    saved recipe html and UA and DB integration

--- assets/javascript/uaDBstore.js
+++ assets/javascript/uaDBstore.js
@@ -42,9 +42,21 @@
    
    // Takes user to saved recipes.
    // Pulls data from the database to run the yummmly get recipe API and generate a list similar to propogating the search list but for each recipeID
-  $(".MySaved").on("click", function(event) {
+  $("document").on("click", "div.mySaved", function(event) {
     event.preventDefault();
+      window.location.href = "savedRecipes.html" //not created yet
       var savedRecipe = childSnapshot.val().recipeId;
+      var queryURL3 = "https://api.yummly.com/v1/api/recipe/"+savedRecipe+"?_app_id=069691a5&_app_key=3944fb993fe2cb009e5e6a5fd1e4facb"
+      $.ajax({
+        url: queryURL3,
+        method: "GET"
+      }).done(function(response) {
+        var smallPic = response.images.hostedSmallUrl; //url for small picture to display
+        var savedName = response.name; //Name of recipe
+        // Make a variable (forked from David's barcode.js) to combine the picture, recipeID, and recipe name into one variable to later display
+        var savedListings = ("<img src="smallPic"><div data-id='"+savedRecipe+"' class='recipeOption'><div class='recipeThumb'>" + savedName + "</div></div></div>");
+        $(".savedRecipe").append(savedListings)
+
     }
   // On the addition of saved recipes to the database
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
