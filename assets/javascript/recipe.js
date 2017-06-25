27ac797f6264bf2965adabf6907858217d148262
davesrose <dsr0116@yahoo.com>
2017-06-25 15:51:35 -0400

    update

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -110,7 +110,7 @@ $(document).ready(function(){
 		    // User is signed in.
 			    recipe.number = recipe.number+1;
 				listRef.set(recipe.number);
-			    listRef.update({
+			    listRef.child(recipe.number).update({
 			        recipeId: recipeID,
 			        recipeName: recipeName
 			    });
