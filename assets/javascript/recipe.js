f2258ea8c024655ef5ce497f31d71c9cbdf6b1a2
davesrose <dsr0116@yahoo.com>
2017-06-27 19:27:12 -0400

    trying again

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
       }
       var cals = Math.round(9*fat+4*carbs+4*protein);
       var recipeSource = response.source.sourceRecipeUrl;
-      recipeSource.replace("http:", "https:");
+      recipeSource.replace("http://", "");
 	  for (var i = 0; i < response.ingredientLines.length; i++) {
       	var ingredients = response.ingredientLines[i]
       	console.log(ingredients);
